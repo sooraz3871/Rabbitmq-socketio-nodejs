e8dc4a78ac843087afaaf7bb617b16984a9ca4da
Suraj Dabadi <shadow-dev@sooraz-mac-pro.local>
2022-07-01 08:39:05 +0545

    added reactjs client

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import { Container } from "@mui/material";
+import React, { Fragment } from "react";
+import Navbar from "./components/navbar.component";
+import AppTable from "./components/table.component";
+import { io } from "socket.io-client";
+import CircularColor from "./components/progress.component";
+
+const App = () => {
+  const [data, setData] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const fetchData = () => {
+      const socket = io("http://localhost:4001");
+      socket.on("random", (rawdata) => {
+        const newData = JSON.parse(rawdata);
+
+        setData((prevprop) => [...prevprop, ...newData]);
+
+        // console.log(newData);
+        setLoading(false);
+      });
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <Fragment>
+      <Navbar />
+      {loading && <CircularColor />}
+      {!loading && <AppTable tabledata={data} />}
+    </Fragment>
+  );
+};
+export default App;
